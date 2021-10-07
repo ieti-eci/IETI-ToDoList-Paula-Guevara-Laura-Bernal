fea4b4c46ac603c738efb3b890a7d7d86c50835f
PaulaSanchez810 <46799459+PaulaSanchez810@users.noreply.github.com>
2021-10-07 15:37:29 -0500

    Add component createTask y handleSubmit

--- src/App.js
+++ src/App.js
@@ -5,11 +5,24 @@ function App() {
   const [ tasks, setTasks ] = useState([
     { isCompleted: false, taskName: 'task name' }]);
 
-  const [ textValue, setValue] = useState("");
+  const [textValue, setValue] = useState("");
 }
 
   const handleChange = (event)=>{
     const textValue = event.target.value;
     setTasks(textValue);
   }
+  
+  const createTask = (name) => {
+    const newTask = {
+      isCompleted: false,
+      name: name,
+    }
+    setTasks([...tasks, newTask])
+  } 
+  
+  const handleSubmit = (event) =>{
+    event.preventDefault();
+    createTask(textValue); 
+  };
 export default App;
