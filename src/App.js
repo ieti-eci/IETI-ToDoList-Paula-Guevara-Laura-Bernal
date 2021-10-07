700a769295ab00bc589a4068823b470daaf70d03
PaulaSanchez810 <paula.guevara-s@mail.escuelaing.edu.co>
2021-10-07 18:48:42 -0500

    Update

--- src/App.js
+++ src/App.js
@@ -1,1 +1,45 @@
 
+import { useState } from 'react';
+
+function App() {
+  const [ tasks, setTasks ] = useState([
+    { isCompleted: false, taskName: 'learn React' }]);
+
+  const [ texValue, setValue] = useState("");
+
+  const createTask = (taskName) => {
+    const newTask = {
+      isCompleted: false,
+      taskName: taskName,
+    }
+    setTasks([...tasks, newTask])
+  } 
+
+  const handleSubmit = (event) =>{
+    event.preventDefault();
+    createTask(texValue); 
+  };
+
+  const handleChange = (event)=>{
+    const taskValue = event.target.value;
+    setValue(taskValue);
+  }
+
+  return (
+    <main>
+			<form onSubmit={handleSubmit}>
+				<input value={texValue} onChange={handleChange} type="text" placeholder="task name" />
+				<button>Create Task</button>
+			</form>
+      <ul>
+				{tasks.map((task, index) => {
+					return (
+						<span>class task item</span>
+					);
+				})}
+			</ul>
+		</main>
+);
+}
+
+export default App;
